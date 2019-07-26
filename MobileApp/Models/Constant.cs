e9ca34f73e17dbd8de6af45d07511bef93b675c3
Nikita <frozmanik@gmail.com>
2019-07-26 16:14:24 +0300

    Add project files.

---
+++ MobileApp/Models/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileApp.Models
+{
+    public class Constant
+    {
+        public static bool isDev = true;
+
+        public static Color BackgroundColor = Color.FromRgb(58, 153, 215);
+        public static Color MaintTextColor = Color.White;
+
+        public static int LoginIconHeight = 120;
+    }
+}
---
+++ MobileApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using MobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        void Init()
+        {
+            BackgroundColor = Constant.BackgroundColor;
+            Lbl_Password.TextColor = Constant.MaintTextColor; 
+            Lbl_Username.TextColor = Constant.MaintTextColor;
+            ActivitySpinner.IsVisible = false;
+            LoginIcon.HeightRequest = Constant.LoginIconHeight;
+
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Username.Completed += (s, e) => SignInProcedure(s,e);
+        }
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login success", "Oke");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login Not Correct", "Oke");
+            }
+        }
+    }
+}
